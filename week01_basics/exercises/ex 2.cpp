4dc4fb89e2973a5de2f872b8137ee12b7cf3ee7a
S0uf12n8 <oulkadisoufiane69@gmail.com>
2026-02-12 15:22:34 +0100

    var declaration

---
+++ week01_basics/exercises/ex 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a;
+    double b;
+    char c;
+    bool first;
+
+    cout<<"give me the value of a :";
+    cin >> a;
+    cout<<"give me the value of b: ";
+    cin >> b;
+    cout <<"give me the value of c : ";
+    cin >>c;
+    cout<<"give me the vlaue of first :";
+    cin >> first ;
+    /*=====print fin values ==========
+
+    cout<<"the value you give for a : "<<a;
+    cout<<"\nthe value you give for b: "<<b;
+    cout<<"\nthe value you give for c :" <<c;
+    cout<<"\nthe value you give for first is : "<< first;*/
+
+    /*we can write also like thise :*/
+
+    cout<<"the value you give for a : "<<a <<"\nthe value you give for b: "<<b<<"\nthe value you give for c :" <<c<<"\nthe value you give for first is : "<< first;
+
+    return 0;
+}
---
+++ week01_basics/exercises/exrcrcie 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a;
+    cout<<"give me a numeber :";
+    cin >>a;
+    cout<<" the value of a you give : " << a;
+    return 0;
+}
