adb0241ebb0fa0ebafdb5719499251e6f080c6c2
S0uf12n8 <oulkadisoufiane69@gmail.com>
2026-02-12 11:50:14 +0100

    first practice

---
+++ week01_basics/exercises/Untitled9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cout<<"entrer deux chiffres : ";
+	cin >>a;
+	cin >>b;
+	c=a+b;
+	
+	cout<<"c=" <<c;
+	cout<<"\n find de traitement... \n\n";
+	return 0;
+}
